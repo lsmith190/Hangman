c23d2ad44ee6ce3162acc0e0d34d298a77de75c1
lsmith190 <lindseyr.smith1217@gmail.com>
2019-02-17 13:38:17 -0500

    replaced underscores with letter value

--- script.js
+++ script.js
@@ -7,7 +7,24 @@
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let arrOfAtl = ['varsity', 'guwop', 'beltline', 'cnn', 'falcons', 'braves', 'marta', 'traffic', 'hawks', 'outkast']
+var indexOfClickLetter;
+var ansArr = [];
 
+var word = arrOfAtl[Math.floor(Math.random() * arrOfAtl.length)];
+console.log(word)
+
+let split = word.split("");
+console.log(split)
+
+
+for (var i = 0; i < word.length; i++) {
+    ansArr.push(" _ ")
+        // ansArr[i] = " _ "
+
+    // let display = document.createTextNode(ansArr[i])
+    // document.body.appendChild(display)
+}
+$(".test").append(ansArr)
 
 function buttons() {
     for (let i = 0; i < alphabet.length; i++) {
@@ -16,15 +33,24 @@ function buttons() {
         alphButton.appendChild(letters)
         document.body.appendChild(alphButton)
 
+
         $(alphButton).on('click', function(event) {
             if (word.indexOf($(event.currentTarget).text()) > -1) {
                 // If statement is checking to see if letter is contained within word. Must include > -1, javacript does not recognize 0 as true
                 this.style.backgroundColor = 'green'
-                let letterDisplay = document.createTextNode(alphabet[i])
-                document.body.appendChild(letterDisplay)
+
+                for (i = 0; i < split.length; i++) {
+                    if ($(event.currentTarget).text() === split[i]) {
+                        ansArr[i] = $(event.currentTarget).text()
+
+                    }
+                    $(".test").text(ansArr.join(" "))
+                }
             } else {
                 this.style.backgroundColor = "red"
+                alert("Nope...try again")
             }
+
         })
 
     }
@@ -32,21 +58,14 @@ function buttons() {
 
 buttons()
 
-
-var word = arrOfAtl[Math.floor(Math.random() * arrOfAtl.length)];
-console.log(word)
-
-
-var ansArr = [];
-for (var i = 0; i < word.length; i++) {
-    ansArr[i] = " _ "
-    let display = document.createTextNode(ansArr[i])
-    document.body.appendChild(display)
+function x(letterClicked) {
+    return split.indexOf(letterClicked)
 }
-console.log(ansArr)
+
+
+
+
+
 
 // If letter button clicked === letter in split word array, replace that underscore with letter value and turn green
-// If else, turn letter button red 
-
-let split = word.split("");
-console.log(split)
+// If else, turn letter button red
